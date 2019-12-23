7a8ae0ab86235c9f78d9365de2c1384aa1416abc
Andriy <andriymattt@gmail.com>
2019-12-23 21:24:31 +0200

    feature/mvc: change output info

--- src/main/java/com/epam/model/State.java
+++ src/main/java/com/epam/model/State.java
@@ -3,18 +3,18 @@ package com.epam.model;
 public interface State {
 
     default void toDo(Task task){
-        System.out.println("toDo - is not allowed");
+        System.out.println("'to do' - is not allowed!");
     }
 
     default void inProgress(Task task){
-        System.out.println("inProgress - is not allowed");
+        System.out.println("'in progress' - is not allowed!");
     }
 
     default void codeReview(Task task){
-        System.out.println("codeReview - is not allowed");
+        System.out.println("'code review' - is not allowed!");
     }
 
     default void done(Task task){
-        System.out.println("done - is not allowed");
+        System.out.println("'done' - is not allowed!");
     }
 }
