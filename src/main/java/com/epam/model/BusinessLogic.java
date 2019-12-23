70a9da31cc14130331053bcfd56546baecb04f43
Andriy <andriymattt@gmail.com>
2019-12-23 21:21:25 +0200

    feature/mvc: add BusinessLogic class and override methods of Model interface

---
+++ src/main/java/com/epam/model/BusinessLogic.java
@@ -0,0 +1,61 @@
+package com.epam.model;
+
+import java.util.List;
+
+public class BusinessLogic implements Model {
+
+    private Domain domain;
+    private Task selectedTask;
+
+    public BusinessLogic() {
+        domain = new Domain();
+    }
+
+    @Override
+    public void createNewTask() {
+        domain.createNewTask();
+    }
+
+    @Override
+    public List<Task> getTaskList() {
+        return domain.getTaskList();
+    }
+
+    @Override
+    public Task getSelectedTask() {
+        return selectedTask;
+    }
+
+    @Override
+    public void setSelectedTask(int index) {
+        selectedTask = getTaskList().get(index);
+    }
+
+    private void changeSelectedTaskState() {
+        domain.setTask(selectedTask.getTaskNumber() - 1, selectedTask);
+    }
+
+    @Override
+    public void toDo() {
+        selectedTask.toDo();
+        changeSelectedTaskState();
+    }
+
+    @Override
+    public void inProgress() {
+        selectedTask.inProgress();
+        changeSelectedTaskState();
+    }
+
+    @Override
+    public void codeReview() {
+        selectedTask.codeReview();
+        changeSelectedTaskState();
+    }
+
+    @Override
+    public void done() {
+        selectedTask.done();
+        changeSelectedTaskState();
+    }
+}
