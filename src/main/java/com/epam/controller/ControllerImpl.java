e0193e9ee9b12495e596ae88c1217f914a1d2655
Andriy <andriymattt@gmail.com>
2019-12-23 21:31:39 +0200

    feature/mvc: add ControllerImpl class and override methods of Controller interface

---
+++ src/main/java/com/epam/controller/ControllerImpl.java
@@ -0,0 +1,55 @@
+package com.epam.controller;
+
+import com.epam.model.BusinessLogic;
+import com.epam.model.Task;
+
+import java.util.List;
+
+public class ControllerImpl implements Controller {
+
+    private BusinessLogic businessLogic;
+
+    public ControllerImpl() {
+        businessLogic = new BusinessLogic();
+    }
+
+    @Override
+    public void createNewTask() {
+        businessLogic.createNewTask();
+    }
+
+    @Override
+    public List<Task> getTaskList() {
+        return businessLogic.getTaskList();
+    }
+
+    @Override
+    public Task getSelectedTask() {
+        return businessLogic.getSelectedTask();
+    }
+
+    @Override
+    public void setSelectedTask(int index) {
+        businessLogic.setSelectedTask(index);
+    }
+
+    @Override
+    public void toDo() {
+        businessLogic.toDo();
+    }
+
+    @Override
+    public void inProgress() {
+        businessLogic.inProgress();
+    }
+
+    @Override
+    public void codeReview() {
+        businessLogic.codeReview();
+    }
+
+    @Override
+    public void done() {
+        businessLogic.done();
+    }
+}
