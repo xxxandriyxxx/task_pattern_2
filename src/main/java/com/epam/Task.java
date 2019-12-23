dda17a5ad8c586fe5005aecf5f5e82095987def9
Andriy <andriymattt@gmail.com>
2019-12-23 17:11:40 +0200

    feature/state: add state field and setState method to the Task class

--- src/main/java/com/epam/Task.java
+++ src/main/java/com/epam/Task.java
@@ -4,10 +4,15 @@ public class Task {
 
     private static int counter = 0;
     private int taskNumber;
+    private State state;
 
     public Task() {
         counter++;
         taskNumber = counter;
+    }
+
+    public void setState(State state) {
+        this.state = state;
     }
 
     @Override
