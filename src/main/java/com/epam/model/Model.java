23000685c1c145ef5fac323bd3cf4e7837c536bd
Andriy <andriymattt@gmail.com>
2019-12-23 21:16:56 +0200

    feature/mvc: add methods to Model interface

--- src/main/java/com/epam/model/Model.java
+++ src/main/java/com/epam/model/Model.java
@@ -1,4 +1,23 @@
 package com.epam.model;
 
+import java.util.List;
+
 public interface Model {
+
+    void createNewTask();
+
+    List<Task> getTaskList();
+
+    Task getSelectedTask();
+
+    void setSelectedTask(int index);
+
+    void toDo();
+
+    void inProgress();
+
+    void codeReview();
+
+    void done();
+
 }
